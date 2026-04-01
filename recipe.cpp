c9c123b62f38ddc84018ee918355bf9d0e4d6c94
camuzin <camiartistic377@gmail.com>
2026-03-31 20:06:14 -0400

    tweak formatting within setters + change name

--- recipe.cpp
+++ recipe.cpp
@@ -4,15 +4,23 @@
 
 // ************ RECIPE ************
 // code by Camila
-Recipe::Recipe(std::string name, std::string category)
-	: m_name{ name }, m_category{ category } {}
+Recipe::Recipe(std::string name, std::string category) {
+	setRecipeName(name);
+	setRecipeCategory(category);
+}
 
 // ERROR HANDLING for setters when strings are empty
 
-void Recipe::setRecipeName(std::string name) { m_name = name; }
+void Recipe::setRecipeName(std::string name) {
+	m_name = name;
+	// implement validation
+} 
 std::string Recipe::getRecipeName() { return m_name; }
 
-void Recipe::setRecipeCategory(std::string category) { m_category = category; }
+void Recipe::setRecipeCategory(std::string category) {
+	m_category = category;
+	// implement validation
+} 
 std::string Recipe::getRecipeCategory() { return m_category; }
 
 
