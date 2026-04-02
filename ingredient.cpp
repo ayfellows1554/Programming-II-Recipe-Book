40b5ed2c23e5036d3e729193f86860b89c2e95ca
kristropica <kristropica@gmail.com>
2026-04-02 18:59:56 -0400

    Upload code to ingredient.cpp

--- ingredient.cpp
+++ ingredient.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+#include "Ingredient.h"
 
+Ingredient::Ingredient(const std::string& amt, const std::string& n)
+    : amount(amt), name(n) {}
+
+void Ingredient::displayIngredient() const {
+    std::cout << amount << " " << name << std::endl;
+}
