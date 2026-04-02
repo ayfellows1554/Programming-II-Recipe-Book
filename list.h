58fb23a67ea24b761f6c5902c488405e927a2cb7
kristropica <kristropica@gmail.com>
2026-04-02 18:13:22 -0400

    Add finished(?) code to list.h

--- list.h
+++ list.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <string>
+#include <vector>
 
+class List {
+private:
+    std::string name;
+    std::vector<std::string> recipes;
+
+public:
+    List(const std::string& listName);
+
+    void createList(const std::string& recipe);
+    void editList(int index, const std::string& newRecipe);
+    void deleteList(int index);
+    void displayLists() const;
+};
