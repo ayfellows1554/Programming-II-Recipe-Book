e6698fcfa827dba0c39547683c100cd0fd00af6e
Grace786 <168041158+Grace786@users.noreply.github.com>
2026-04-02 19:22:57 -0400

    Add Ingredient class definition
    Define Ingredient class with constructor and display method.

--- ingredient.h
+++ ingredient.h
@@ -1,1 +1,13 @@
+#pragma once
+#include <string>
 
+class Ingredient {
+public:
+    Ingredient(const std::string& amount, const std::string& name);
+
+    void displayIngredient() const;
+
+private:
+    std::string amount;
+    std::string name;
+};
