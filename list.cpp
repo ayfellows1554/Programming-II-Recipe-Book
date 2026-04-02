2bffd59c36def089e1cd1551821fbe1fbef1442b
kristropica <kristropica@gmail.com>
2026-04-02 18:16:06 -0400

    Updated list.cpp with complete code.

--- list.cpp
+++ list.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include "List.h"
 
+List::List(const std::string& listName)
+    : name(listName) {}
+
+void List::createList(const std::string& recipe) {
+    recipes.push_back(recipe);
+}
+
+void List::editList(int index, const std::string& newRecipe) {
+    if (index >= 0 && index < recipes.size()) {
+        recipes[index] = newRecipe;
+    }
+}
+
+void List::deleteList(int index) {
+    if (index >= 0 && index < recipes.size()) {
+        recipes.erase(recipes.begin() + index);
+    }
+}
+
+void List::displayLists() const {
+    std::cout << "List: " << name << std::endl;
+    for (const auto& recipe : recipes) {
+        std::cout << "- " << recipe << std::endl;
+    }
+}
