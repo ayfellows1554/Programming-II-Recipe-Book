b0bb82819efe8dc896b203c2ddf52e53568d2223
Gabriela <gd04640@gmail.com>
2026-04-03 12:36:01 -0400

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,26 +1,27 @@
-// Recipe Book driver (main code)
 #include <iostream>
 #include "recipe.h"
 #include "list.h"
 
 using namespace std;
 
-// placeholder functions will beim plemented later
+// Placeholder functions (to be implemented later)
 void createRecipe() {
+    cout << "Create Recipe feature...\n";
 }
 
 void searchRecipes() {
+    cout << "Search Recipe feature...\n";
 }
 
 void viewLists() {
+    cout << "View Lists feature...\n";
 }
-
+// basic menu for now
 void showMenu() {
-
-    return 0;
+    cout << "\n****** Recipe Manager ******\n";
+    cout << "1. Create Recipe\n";
+    cout << "2. Search Recipes\n";
+    cout << "3. View Lists\n";
+    cout << "0. Exit\n";
+    cout << "Enter your choice: ";
 }
-
-
-
-
- 
