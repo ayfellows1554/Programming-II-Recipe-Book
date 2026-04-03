ce3bd5bfada72151703636dddcd2023cf7a2e4eb
Gabriela <gd04640@gmail.com>
2026-04-03 18:21:12 -0400

    Added main function

--- driver.cpp
+++ driver.cpp
@@ -25,3 +25,34 @@ void showMenu() {
     cout << "0. Exit\n";
     cout << "Enter your choice: ";
 }
+
+int main() {
+    int choice;
+
+    do {
+        showMenu();
+        cin >> choice;
+// makes sure space is ignored
+        cin.ignore(1000, '\n');
+
+        switch (choice) {
+            case 1:
+                createRecipe();
+                break;
+            case 2:
+                searchRecipes();
+                break;
+            case 3:
+                viewLists();
+                break;
+            case 0:
+                cout << "Thank you for using our Recipe Book!\n";
+                break;
+            default:
+                cout << "Invalid choice. Try again.\n";
+        }
+
+    } while (choice != 0);
+
+    return 0;
+}
