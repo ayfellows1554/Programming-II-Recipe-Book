0729acd309dc0a5c7c324497527729146912371f
Grace786 <168041158+Grace786@users.noreply.github.com>
2026-04-04 18:43:32 -0400

    Update ListManager.h

--- ListManager.h
+++ ListManager.h
@@ -8,10 +8,14 @@ private:
     std::vector<List> m_lists;
 
 public:
+    //Create List
     void createList(const std::string& name);
+    //Delete List
     bool deleteList(const std::string& name);
 
     List* getList(const std::string& name);
 
+    //Display List
     void displayAllLists() const;
+   
 };
