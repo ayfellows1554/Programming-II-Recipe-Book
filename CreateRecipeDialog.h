82cd9abf8945a6ff81960c5909516bbe5a139fea
Grace786 <168041158+Grace786@users.noreply.github.com>
2026-04-22 14:13:56 -0400

    Update CreateRecipeDialog.h

--- CreateRecipeDialog.h
+++ CreateRecipeDialog.h
@@ -1,6 +1,14 @@
 #pragma once
-#include <wx/wx.h>
-#include "RecipeBook.h"
+#include <memory>
+#include <wx/spinctrl.h>
+#include <wx/scrolwin.h>
+#include <wx/listbox.h>
+#include <wx/choice.h>
+
+#include"Recipe.h"
+
+//forward declaration
+class RecipeBook;
 
 class CreateRecipeDialog : public wxDialog
 {
@@ -9,17 +17,29 @@ public:
 
 private:
     RecipeBook& m_book;
+    Recipe m_recipe;
 
     wxTextCtrl* txtName;
     wxTextCtrl* txtCategory;
     wxSpinCtrl* spinServings;
     wxSpinCtrl* spinCookTime;
 
+    wxTextCtrl* txtIngName;
+    wxTextCtrl* txtIngAmount;
+    wxTextCtrl* txtIngUnit;
+    wxChoice* choiceIngredientType;
+
     wxListBox* ingredientList;
     wxListBox* instructionList;
 
+    // Ingredient handlers
     void OnAddIngredient(wxCommandEvent& evt);
+    void OnEditIngredient(wxCommandEvent& evt);     // ⭐ NEW
+    void OnDeleteIngredient(wxCommandEvent& evt);   // ⭐ NEW
+
+    // Instruction handlers
     void OnAddInstruction(wxCommandEvent& evt);
+
+    // Save
     void OnSave(wxCommandEvent& evt);
 };
-
