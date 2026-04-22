2dbced0557fd7e281ea96229118880fef9ae0b22
Grace786 <168041158+Grace786@users.noreply.github.com>
2026-04-22 14:17:16 -0400

    Update ListManagerDialog.cpp

--- ListManagerDialog.cpp
+++ ListManagerDialog.cpp
@@ -1,60 +1,77 @@
 #include "ListManagerDialog.h"
+#include "Theme.h"
+#include "ModifyRecipeDialog.h"
 
 enum
 {
-	// Unique IDs for the buttons in the list manager dialog
     ID_CreateList = 3001,
     ID_DeleteList,
     ID_ViewList,
     ID_AddRecipe,
-    ID_RemoveRecipe
+    ID_RemoveRecipe,
+    ID_ModifyRecipe   // ⭐ NEW
 };
 
-// Constructor for the list manager dialog
 ListManagerDialog::ListManagerDialog(wxWindow* parent, RecipeBook& book, ListManager& manager)
     : wxDialog(parent, wxID_ANY, "List Manager", wxDefaultPosition, wxSize(600, 500)),
     m_book(book), m_manager(manager)
 {
+    PastelTheme::ApplyTheme(this);
+
     wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
 
+    wxStaticText* header = new wxStaticText(this, wxID_ANY, "Manage Your Lists");
+    PastelTheme::StyleHeader(header);
+    mainSizer->Add(header, 0, wxALIGN_CENTER | wxTOP | wxBOTTOM, 10);
+
     listBox = new wxListBox(this, wxID_ANY);
-    mainSizer->Add(listBox, 1, wxEXPAND | wxALL, 5);
+    mainSizer->Add(listBox, 1, wxEXPAND | wxALL, 10);
 
     wxBoxSizer* btnSizer = new wxBoxSizer(wxHORIZONTAL);
 
-	// Create buttons for list management and add them to the button sizer
-    btnSizer->Add(new wxButton(this, ID_CreateList, "Create List"), 1, wxALL, 5);
-    btnSizer->Add(new wxButton(this, ID_DeleteList, "Delete List"), 1, wxALL, 5);
-    btnSizer->Add(new wxButton(this, ID_ViewList, "View Contents"), 1, wxALL, 5);
-    btnSizer->Add(new wxButton(this, ID_AddRecipe, "Add Recipe"), 1, wxALL, 5);
-    btnSizer->Add(new wxButton(this, ID_RemoveRecipe, "Remove Recipe"), 1, wxALL, 5);
+    wxButton* btnCreate = new wxButton(this, ID_CreateList, "Create List");
+    wxButton* btnDelete = new wxButton(this, ID_DeleteList, "Delete List");
+    wxButton* btnView = new wxButton(this, ID_ViewList, "View Contents");
+    wxButton* btnAdd = new wxButton(this, ID_AddRecipe, "Add Recipe");
+    wxButton* btnRemove = new wxButton(this, ID_RemoveRecipe, "Remove Recipe");
+    wxButton* btnModify = new wxButton(this, ID_ModifyRecipe, "Modify Recipe"); // ⭐ NEW
+
+    PastelTheme::StyleButton(btnCreate);
+    PastelTheme::StyleButton(btnDelete);
+    PastelTheme::StyleButton(btnView);
+    PastelTheme::StyleButton(btnAdd);
+    PastelTheme::StyleButton(btnRemove);
+    PastelTheme::StyleButton(btnModify);
+
+    btnSizer->Add(btnCreate, 1, wxALL, 5);
+    btnSizer->Add(btnDelete, 1, wxALL, 5);
+    btnSizer->Add(btnView, 1, wxALL, 5);
+    btnSizer->Add(btnAdd, 1, wxALL, 5);
+    btnSizer->Add(btnRemove, 1, wxALL, 5);
+    btnSizer->Add(btnModify, 1, wxALL, 5); // ⭐ NEW
 
     mainSizer->Add(btnSizer, 0, wxEXPAND);
 
-	// Bind button events to their respective handlers
     Bind(wxEVT_BUTTON, &ListManagerDialog::OnCreateList, this, ID_CreateList);
     Bind(wxEVT_BUTTON, &ListManagerDialog::OnDeleteList, this, ID_DeleteList);
     Bind(wxEVT_BUTTON, &ListManagerDialog::OnViewList, this, ID_ViewList);
     Bind(wxEVT_BUTTON, &ListManagerDialog::OnAddRecipe, this, ID_AddRecipe);
     Bind(wxEVT_BUTTON, &ListManagerDialog::OnRemoveRecipe, this, ID_RemoveRecipe);
+    Bind(wxEVT_BUTTON, &ListManagerDialog::OnModifyRecipe, this, ID_ModifyRecipe); // ⭐ NEW
 
     SetSizer(mainSizer);
-
     RefreshLists();
 }
 
-// Refresh the list box with the current lists from the ListManager
 void ListManagerDialog::RefreshLists()
 {
     listBox->Clear();
-
     const auto& lists = m_manager.getLists();
-    for (size_t i = 0; i < lists.size(); ++i)
-        listBox->Append(lists[i].getListName());
+    for (const auto& list : lists)
+        listBox->Append(list.getListName());
 }
 
-// Event handler for creating a new list
-void ListManagerDialog::OnCreateList(wxCommandEvent& evt)
+void ListManagerDialog::OnCreateList(wxCommandEvent&)
 {
     wxTextEntryDialog dlg(this, "Enter new list name:");
     if (dlg.ShowModal() == wxID_OK)
@@ -69,46 +86,36 @@ void ListManagerDialog::OnCreateList(wxCommandEvent& evt)
     }
 }
 
-// Event handler for deleting the selected list
-void ListManagerDialog::OnDeleteList(wxCommandEvent& evt)
+void ListManagerDialog::OnDeleteList(wxCommandEvent&)
 {
     int sel = listBox->GetSelection();
     if (sel == wxNOT_FOUND) return;
 
-    std::string name = listBox->GetString(sel).ToStdString();
-    m_manager.deleteList(name);
+    m_manager.deleteList(listBox->GetString(sel).ToStdString());
     RefreshLists();
 }
 
-// Event handler for viewing the contents of the selected list
-void ListManagerDialog::OnViewList(wxCommandEvent& evt)
+void ListManagerDialog::OnViewList(wxCommandEvent&)
 {
     int sel = listBox->GetSelection();
     if (sel == wxNOT_FOUND) return;
 
-    std::string name = listBox->GetString(sel).ToStdString();
-    List* list = m_manager.getList(name);
+    List* list = m_manager.getList(listBox->GetString(sel).ToStdString());
+    if (!list) return;
 
-    if (!list) {
-        wxMessageBox("List not found.");
-        return;
-    }
-
-    wxString output = "List: " + wxString::FromUTF8(list->getListName().c_str()) + "\n\n";
+    wxString output = "List: " + list->getListName() + "\n\n";
     for (int i = 0; i < list->size(); i++)
-        output += "- " + wxString::FromUTF8(list->getRecipe(i).c_str()) + "\n";
+        output += "- " + list->getRecipe(i) + "\n";
 
-    wxMessageBox(output);
+    wxMessageBox(output, "List Contents");
 }
 
-// Event handler for adding a recipe to the selected list
-void ListManagerDialog::OnAddRecipe(wxCommandEvent& evt)
+void ListManagerDialog::OnAddRecipe(wxCommandEvent&)
 {
     int sel = listBox->GetSelection();
     if (sel == wxNOT_FOUND) return;
 
-    std::string listName = listBox->GetString(sel).ToStdString();
-    List* list = m_manager.getList(listName);
+    List* list = m_manager.getList(listBox->GetString(sel).ToStdString());
 
     wxTextEntryDialog dlg(this, "Enter recipe name to add:");
     if (dlg.ShowModal() == wxID_OK)
@@ -130,14 +137,12 @@ void ListManagerDialog::OnAddRecipe(wxCommandEvent& evt)
     }
 }
 
-// Event handler for removing a recipe from the selected list
- void ListManagerDialog::OnRemoveRecipe(wxCommandEvent& evt)                     
+void ListManagerDialog::OnRemoveRecipe(wxCommandEvent&)
 {
     int sel = listBox->GetSelection();
     if (sel == wxNOT_FOUND) return;
 
-    std::string listName = listBox->GetString(sel).ToStdString();
-    List* list = m_manager.getList(listName);
+    List* list = m_manager.getList(listBox->GetString(sel).ToStdString());
 
     wxTextEntryDialog dlg(this, "Enter recipe name to remove:");
     if (dlg.ShowModal() == wxID_OK)
@@ -153,7 +158,29 @@ void ListManagerDialog::OnAddRecipe(wxCommandEvent& evt)
             }
         }
 
-		// Error handling if recipe is not found in the list
         wxMessageBox("Recipe not found in this list.");
     }
+}
+
+void ListManagerDialog::OnModifyRecipe(wxCommandEvent&)   // ⭐ NEW
+{
+    int sel = listBox->GetSelection();
+    if (sel == wxNOT_FOUND) return;
+
+    wxTextEntryDialog dlg(this, "Enter recipe name to modify:");
+    if (dlg.ShowModal() != wxID_OK)
+        return;
+
+    std::string recipeName = dlg.GetValue().ToStdString();
+    Recipe* recipe = m_book.findRecipe(recipeName);
+
+    if (!recipe)
+    {
+        wxMessageBox("Recipe not found in RecipeBook.");
+        return;
+    }
+
+    ModifyRecipeDialog modifyDlg(this, *recipe);
+    if (modifyDlg.ShowModal() == wxID_OK)
+        wxMessageBox("Recipe updated successfully.");
 }
