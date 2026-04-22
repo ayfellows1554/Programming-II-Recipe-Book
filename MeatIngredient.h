6c6adcf83deb6add0438df9172351c1a467721e3
Grace786 <168041158+Grace786@users.noreply.github.com>
2026-04-22 14:45:41 -0400

    Update MeatIngredient.h

--- MeatIngredient.h
+++ MeatIngredient.h
@@ -2,7 +2,7 @@
 #include "Ingredient.h"
 #include <iostream>
 
-//One of the derived classes
+//One of the derived classes for IngredientBase
 class MeatIngredient : public Ingredient {
 public:
     using Ingredient::Ingredient;
