5a6d0c19dc2854b587e20c9295026a54ce90fcef
Grace786 <168041158+Grace786@users.noreply.github.com>
2026-04-22 14:28:06 -0400

    Update ViewRecipeDialog.h

--- ViewRecipeDialog.h
+++ ViewRecipeDialog.h
@@ -1,12 +1,11 @@
 #pragma once
 #include <wx/wx.h>
+#include <wx/sizer.h>
+#include <wx/scrolwin.h>
 #include "Recipe.h"
 
 class ViewRecipeDialog : public wxDialog
 {
 public:
     ViewRecipeDialog(wxWindow* parent, const Recipe& recipe);
-
-private:
-    void BuildLayout(const Recipe& recipe);
 };
