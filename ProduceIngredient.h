0b77754a3d1d17bf462d036d4a38ed8185c1c47a
Grace786 <168041158+Grace786@users.noreply.github.com>
2026-04-22 14:45:58 -0400

    Update ProduceIngredient.h

--- ProduceIngredient.h
+++ ProduceIngredient.h
@@ -2,7 +2,7 @@
 #include "Ingredient.h"
 #include <iostream>
 
-//One of the  derived classes for inheritance
+//One of the  derived classes for IngredientBase
 class ProduceIngredient : public Ingredient {
 public:
     using Ingredient::Ingredient; // inherit constructor
